b6afc2a33ff169161e49bfdd548c2db6027c3e21
chrisYiangou <chrisyiangou100@gmail.com>
2024-02-02 10:40:15 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+//Get ID
+const inputArea = document.getElementById("text-input");
+const submitButton = document.getElementById("check-btn");
+const resultDiv = document.getElementById("result");
+
+
+//Functions
+const solvePalindrome = () => {  
+    const isTextAreaEmpty = () => {
+        if(inputArea.value.trim() === "") {
+          alert("Please input a value")
+          return false;
+        }
+          return true;
+      }
+      const removePunctuation = (text) => {
+        return text.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~() \s]/g, " ").trim();
+      }
+      const reverseWord = (text) => {
+        return text.split("").reverse().join("");
+      }
+      const removeSpace = (text) => {
+        return text.replace(/\s/g, "");
+      }
+ //First make sure if text area is not null
+    const isValid = isTextAreaEmpty(); 
+    if (isValid) {
+        let answer = ""
+        const initialValue = removeSpace(removePunctuation(inputArea.value));
+        const reversedValue = removeSpace(removePunctuation(reverseWord(inputArea.value)));
+        console.log(initialValue, reversedValue);
+        if (initialValue === reversedValue) {
+            answer = `${inputArea.value} is a palindrome`
+            resultDiv.innerHTML = answer ;
+        } else {
+            answer = `${inputArea.value} is not a palindrome`
+            resultDiv.innerHTML = answer;
+        }
+    }
+    }
+   
+//Query Selector
+submitButton.addEventListener('click', solvePalindrome);
+
+  
