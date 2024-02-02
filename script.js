b3d630bc9419e0909be9d8fba6709bdcdd1b480d
chrisYiangou <chrisyiangou100@gmail.com>
2024-02-02 13:37:35 +0000

    Added local storage retrival

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const inputArea = document.getElementById("text-input");
 const submitButton = document.getElementById("check-btn");
 const resultDiv = document.getElementById("result");
-
+const historyDiv = document.querySelector("#history")
+const historyButton = document.querySelector("#history-button")
 
 //Functions
 const solvePalindrome = () => {  
@@ -22,22 +23,43 @@ const solvePalindrome = () => {
       const removeSpace = (text) => {
         return text.replace(/\s/g, "");
       }
+      const saveAnswerToMemory = (answer) => {
+        //Will save to local memory for now
+        localStorage.setItem(localStorage.length, answer);
+      }
     const isValid = isTextAreaEmpty(); 
     if (isValid) {
         let answer = ""
         const initialValue = removeSpace(removePunctuation(inputArea.value));
         const reversedValue = removeSpace(removePunctuation(reverseWord(inputArea.value)));
         if (initialValue === reversedValue) {
-            answer = `${inputArea.value} is a palindrome`
+            answer = `${inputArea.value} is a palindrome ✅`;
             resultDiv.innerHTML = answer ;
         } else {
-            answer = `${inputArea.value} is not a palindrome`
+            answer = `${inputArea.value} is not a palindrome ❌`;
             resultDiv.innerHTML = answer;
         }
+        saveAnswerToMemory(answer);
     }
+    } 
+
+    const showHistory = () => {
+        if (!historyDiv.innerHTML) {
+            for (let i=0; i < localStorage.length; i++) {
+                const para =document.createElement("p");
+                para.innerText = localStorage.getItem(i)
+                historyDiv.appendChild(para);
+            }
+        } else {
+            historyDiv.innerHTML = "";
+        }
     }
    
-//Query Selector
+//Query Selectors
 submitButton.addEventListener('click', solvePalindrome);
+inputArea.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') solvePalindrome();
+});
+historyButton.addEventListener('click', showHistory);
 
   
